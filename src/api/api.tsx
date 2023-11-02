599f041d3a3e79bef1fa3da4ac2f6eafcd8dc194
spearker <scchosam132@gmail.com>
2023-11-02 17:32:39 +0900

    FIX: package.json port change

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,9 +1,11 @@
-interface fetchProps {
+const FINYL_API = ''
 
-}
+type noAuthFetchType<T> = (api: string, method: 'GET' | 'POST') => Promise<T>
 
-const customFetch = async () => {
-    await fetch('', {
-
+const noAuthFetch: noAuthFetchType<any> = async (api, method) => {
+    const result = await fetch(`${FINYL_API}/${api}`, {
+        method: method
     })
+
+    return result
 }
