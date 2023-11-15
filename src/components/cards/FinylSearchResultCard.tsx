d5c0ca0756eab39b74a9f586fa6e665e2ae61fc3
spearker <scchosam132@gmail.com>
2023-11-15 14:19:52 +0900

    UPDATE: search card

--- src/components/cards/FinylSearchResultCard.tsx
+++ src/components/cards/FinylSearchResultCard.tsx
@@ -3,10 +3,11 @@ import {NextPage} from "next";
 import {useRouter} from "next/navigation";
 import SearchBox from "@/components/searchBox/SearchBox";
 import {useState} from "react";
-import {IconButton} from "@chakra-ui/react";
+import {IconButton, Skeleton, SkeletonText} from "@chakra-ui/react";
 import {IoClose} from "react-icons/io5";
 import ResultItem from "@/components/ResultItem";
 import {noAuthFetch} from "@/api/api";
+import {Icon} from "@chakra-ui/icons";
 
 interface props {
     currentLocation: string
@@ -18,24 +19,27 @@ interface props {
 const initStyle: string = 'absolute z-20 left-0 bottom-0 h-[95vh] w-[296px] bg-white rounded-t-[9px] border border-black/50-200'
 
 const FinylSearchResultCard: NextPage<props> = ({}) => {
+    const [isSearch, setIsSearch] = useState<boolean>(false)
     const [keyword, setKeyword] = useState<string>("")
+    const [searchKeyword, setSearchKeyword] = useState<string>("")
     const [searchResult, setSearchResult] = useState<Array<storeInfoType>>([])
 
     const searchStore = async () => {
         const results = await noAuthFetch(`search?keyword=${keyword}`, 'GET')
 
-        console.log(results)
+        setIsSearch(true)
+        setSearchKeyword(keyword)
         setSearchResult([...results])
     }
 
     return <div className={'absolute z-10 left-20 top-0 h-screen w-[296px] bg-black/50'}>
-        <div className={`${initStyle} p-6`}>
+        <div className={`${initStyle} px-6 pt-4`}>
             <div>
                 <div className={'w-full h-[140px]'}>
                     <div className={'flex justify-end'}>
                         <IconButton
                             aria-label={''}
-                            icon={<IoClose />}
+                            icon={<Icon aria-label={'x'} as={IoClose} boxSize={21} />}
                             variant={'ghost'}
                             size={'sm'}
                             className={'mb-3'}
@@ -46,22 +50,50 @@ const FinylSearchResultCard: NextPage<props> = ({}) => {
                         setKeyword={setKeyword}
                         placeholder={'레코드샵 이름 검색'}
                         onSearchEvent={() => searchStore()}
+                        isSearch={isSearch}
+                        searchClearEvent={() => {
+                            setIsSearch(false)
+                            setSearchKeyword('')
+                            setKeyword('')
+                        }}
                     />
-                    <div className={'flex flex-col gap-[3px] pt-9'}>
-                        <p className={'font-inter text-gray-900 text-xl font-bold'}>
-                            {`'${keyword}' 에 대한 검색 결과`}
-                        </p>
-                        <p className={'font-inter text-slate-500 text-sm font-normal leading-tight'}>
-                            {`${searchResult.length}개 결과`}
-                        </p>
-                    </div>
-                    <div className={'py-3'}>
-                        {
-                            searchResult.map(store => {
-                                return <ResultItem key={store.id} store={store} />
-                            })
-                        }
-                    </div>
+                    {
+                        isSearch && <>
+                            <div className={'flex flex-col gap-[3px] pt-9'}>
+                                <p className={'font-inter text-gray-900 text-xl font-bold'}>
+                                    {`'${searchKeyword}' 에 대한 검색 결과`}
+                                </p>
+                                <p className={'font-inter text-slate-500 text-sm font-normal leading-tight'}>
+                                    {searchResult.length ? `${searchResult.length}개 결과` : '검색 결과가 없습니다.'}
+                                </p>
+                            </div>
+                            {
+                                searchResult.length ? <div className={'py-3'}>
+                                    {
+                                        searchResult.map(store => {
+                                            return <ResultItem key={store.id} store={store} />
+                                        })
+                                    }
+                                </div> : <></>
+                            }
+                        </>
+                    }
+                    {
+                        !isSearch && <>
+                            <div className={'flex flex-col gap-[3px] pt-9'}>
+                                <p className={'font-inter text-slate-500 text-sm font-normal leading-tight'}>
+                                    검색을 원하시는 레코드샵 이름을 입력해 주세요.
+                                </p>
+                                <div className={'border-y border-gray-200 w-full py-3 mt-3'}>
+                                    <div className={'flex justify-between items-center h-[78px]'}>
+                                        <Skeleton boxSize={78}/>
+                                        <SkeletonText className={'w-[140px] h-fit items-center'} boxSize={'sm'}/>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </>
+                    }
                 </div>
             </div>
         </div>
--- src/components/searchBox/SearchBox.tsx
+++ src/components/searchBox/SearchBox.tsx
@@ -1,26 +1,26 @@
 "use client"
 import {NextPage} from "next";
 import {useRouter} from "next/navigation";
-import {InputGroup, InputLeftElement, Input, InputRightElement} from "@chakra-ui/input";
+import {InputGroup, InputLeftElement, Input, InputRightElement, InputRightAddon} from "@chakra-ui/input";
 import {Icon} from "@chakra-ui/icons";
 import {BsSearch} from "react-icons/bs";
-import {IoSearch} from "react-icons/io5";
+import {IoCloseCircle, IoSearch} from "react-icons/io5";
+import {IconButton} from "@chakra-ui/react";
 
 interface props {
     keyword: string
     setKeyword: (keyword: string) => void
     placeholder?: string
     onSearchEvent?: () => void
+    isSearch?: boolean
+    searchClearEvent?: () => void
 }
 
-const SearchBox: NextPage<props> = ({keyword, setKeyword, placeholder, onSearchEvent}) => {
+const SearchBox: NextPage<props> = ({keyword, setKeyword, placeholder, onSearchEvent, isSearch, searchClearEvent}) => {
     const router = useRouter()
 
     return <div>
         <InputGroup className={'border-gray-400'}>
-            <InputRightElement pointerEvents={'none'}>
-                <Icon as={IoSearch} />
-            </InputRightElement>
             <Input
                 size={'md'}
                 value={keyword}
@@ -28,11 +28,24 @@ const SearchBox: NextPage<props> = ({keyword, setKeyword, placeholder, onSearchE
                     setKeyword(e.target.value)
                 }}
                 onKeyDown={(e) => {
+                    console.log(e.key)
                     if(e.key === 'Enter') {
                         onSearchEvent && onSearchEvent()
                     }
+
+                    if(e.key === 'Escape') {
+                        searchClearEvent && searchClearEvent()
+                    }
                 }}
             />
+            <InputRightElement>
+                {
+                    isSearch && <Icon color={'gray.400'} className={'cursor-pointer'} as={IoCloseCircle} onClick={() => searchClearEvent && searchClearEvent()} />
+                }
+                {
+                    !isSearch && <Icon className={'cursor-pointer'} as={IoSearch} onClick={() => onSearchEvent && onSearchEvent()} />
+                }
+            </InputRightElement>
         </InputGroup>
     </div>
 }
